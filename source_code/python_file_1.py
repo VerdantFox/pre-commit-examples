6d10fa3939165cc38d47446b0fb778f3ace1ec5a
Teddy Williams <twilliams@opentext.com>
2021-09-14 08:55:00 -0600

    added source_code

---
+++ source_code/python_file_1.py
@@ -0,0 +1,10 @@
+"""Do some things"""
+
+
+def add_two_numbers(first, second):
+    """Adds together two numbers"""
+    return first + second
+
+
+if __name__ == "__main__":
+    print(add_two_numbers(2, 5))
---
+++ source_code/python_file_2.py
@@ -0,0 +1,10 @@
+"""Do some other things"""
+
+
+def multiply_two_numbers(first, second):
+    """Multiplies two numbers together"""
+    return first * second
+
+
+if __name__ == "__main__":
+    print(multiply_two_numbers(2, 5))
